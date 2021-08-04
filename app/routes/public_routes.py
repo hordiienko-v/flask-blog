bbe9dfe456b1ed86798aed17c13ffe081bbe7cea
Joseph Lingenberg <lingenberg.joseph@gmail.com>
2021-08-04 14:17:55 +0300

    Modified sign-up

--- app/routes/public_routes.py
+++ app/routes/public_routes.py
@@ -1,12 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/sign-up")
+@app.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    data = request.form
+    print(data)
+    if request.method == "POST":
+        if data['username'] == 'admin' and data['password'] == 'admin':
+            print('wow')
+            return redirect("/")
+        else:
+            return render_template("sign_up.html", error="Incorrect login or password")
     return render_template("sign_up.html")
 
 @app.route("/about")
