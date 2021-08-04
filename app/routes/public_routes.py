157d2a0c635ec8c56e9242fdaec3c36a0a6438ad
Joseph Lingenberg <lingenberg.joseph@gmail.com>
2021-08-04 13:51:15 +0300

    Added html pages for routes 'home', 'about', 'sign-up'

--- app/routes/public_routes.py
+++ app/routes/public_routes.py
@@ -1,13 +1,14 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 def home():
-    return "Home page"
+    return render_template("home.html")
 
 @app.route("/sign-up")
 def sign_up():
-    return "Sign-up"
+    return render_template("sign_up.html")
 
 @app.route("/about")
 def about():
-    return "About"
+    return render_template("about.html")
