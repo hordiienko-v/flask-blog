05a411240f9ceac5c0e3a4a4db4b82f11de58726
Joseph Lingenberg <lingenberg.joseph@gmail.com>
2021-08-08 15:27:23 +0300

    Added 'change email' field

--- app/routes/private_routes.py
+++ app/routes/private_routes.py
@@ -21,8 +21,16 @@ def profile():
 @app.route("/change_bio", methods=["POST"])
 def change_bio():
     new_bio = request.get_json()["bio"]
-
     current_user.bio = new_bio
+    current_user.save_to_db()
+
+    return {"message": "OK"}
+
+@login_required
+@app.route("/change_email", methods=["POST"])
+def change_email():
+    new_email = request.get_json()["email"]
+    current_user.email = new_email
     current_user.save_to_db()
 
     return {"message": "OK"}
