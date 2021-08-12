e54eb5e4cb630a2eced129906d3861f9739b252e
Joseph Lingenberg <lingenberg.joseph@gmail.com>
2021-08-12 10:47:54 +0300

    Changed email length

--- app/models/user.py
+++ app/models/user.py
@@ -7,7 +7,7 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
-    email = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(40), unique=True)
     bio = db.Column(db.String(100))
     password = db.Column(db.String())
 
