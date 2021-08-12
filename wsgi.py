6ac10ccc7a4c082b69908857a692daa37c4400b4
Joseph Lingenberg <lingenberg.joseph@gmail.com>
2021-08-12 10:34:16 +0300

    Changed code in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,22 +2,21 @@ from app import app
 from flask_login import LoginManager
 from app.models.user import User
 
-if __name__ == "__main__":
-    from app.db import db
-    db.init_app(app)
+from app.db import db
+db.init_app(app)
 
-    login_manager = LoginManager()
-    login_manager.login_view = "/sign-in"
-    login_manager.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = "/sign-in"
+login_manager.init_app(app)
 
-    @app.template_filter("format_date")
-    def format_date(value, format="%H:%M %d.%m.%y"):
-        return value.strftime(format)
+@app.template_filter("format_date")
+def format_date(value, format="%H:%M %d.%m.%y"):
+    return value.strftime(format)
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
+@app.before_first_request
+def create_tables():
+    db.create_all()
